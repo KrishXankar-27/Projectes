b4b00eff60df38c9e53bed979e3600083989e62d
KrishXankar <ihavenoenemies.27.thorfi9@gmail.com>
2024-09-07 09:00:21 +0530

    xersise from 6 day to 8

---
+++ js(exersise)/chapter1.js
@@ -0,0 +1,37 @@
+//  create a variable of type string and key to add a number to it.
+
+// 1
+const a = "halo";
+
+const b = 5;
+
+console.log(a+b);
+
+// 2
+console.log(typeof a);
+
+// 3
+const obj = {
+    name : "koko",
+    age : 50
+}
+
+console.log(typeof obj);
+
+// 4
+const obj2 = {
+    ...obj,
+    email : "jaksixols@gmail.com",
+}
+
+// 5
+console.log(obj2);
+
+const obj3 = {
+    apple : "fruite",
+    babana : "fruite",
+    avocado : "fruite",
+    cherry : "fruite",
+}
+
+console.log(obj3);
---
+++ js(exersise)/chapter2.js
@@ -0,0 +1,78 @@
+// 1
+// const age = 50;
+
+// if (age < 10) {
+//     console.log("you are young");
+    
+// } else {
+//     console.log("you can vote");
+    
+// }
+
+// 2
+// const day = "monday";
+
+// switch (day) {
+//     case "monday":
+//         console.log("time to working");
+        
+//         break;
+//     case "tuesday":
+//         console.log("time to working");
+        
+//         break;
+//     case "wenesday":
+//         console.log("time to working");
+        
+//         break;
+//     case "thursday":
+//         console.log("time to working");
+        
+//         break;
+//     case "friday":
+//         console.log("time to working");
+        
+//         break;
+//     case "saturday":
+//         console.log("time to rest");
+        
+//         break;
+//     case "sunday":
+//         console.log("time to rtest");
+        
+//         break;
+
+//     default:
+//         console.log("inva;lied day");
+        
+//         break;
+// }
+
+// 4
+const nums = 30;
+
+if (nums%2 ==0 ) {
+    console.log("number is disible by 2");
+    
+}
+else if (nums%3 ==0) {
+    console.log("number is disible by 3");
+    
+}
+
+
+else {
+console.log("number is divisle by other number");
+
+}
+
+// 3
+const nums1 = 30;
+
+if (nums1 % 2 ==0 && nums1 % 3 == 0) {
+    console.log("number is disible by 2 and 3")
+}
+else {
+    console.log("number is not disible by 2 and 3");
+    
+}
